81de527a9ffc90dad021be5ae6cef2764fd75950
ArthurF28 <arthur.oferreira@gmail.com>
2026-04-04 21:55:34 -0300

    início da função do Modal, definindo os dias mostrados, e a possibilidade de mais de uma tarefa no mesmo dia

--- src/components/ui/PlannerModal.tsx
+++ src/components/ui/PlannerModal.tsx
@@ -4,7 +4,6 @@ import CloseIcon from '@mui/icons-material/Close'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import PendingIcon from '@mui/icons-material/Pending'
 import AppSubjectsTags from './AppSubjectsTags'
-import { Pending } from '@mui/icons-material'
 
 type Status = 'done' | 'pending' | 'adjust'
 
@@ -26,10 +25,45 @@ interface PlannerModalProps {
     tasks: Task[]
 }
 
-function getTaskIcon(status: task['status']) {
+const weekOrder = [
+    'segunda-feira',
+    'terça-feira',
+    'quarta-feira',
+    'quinta-feira',
+    'sexta-feira',
+    'sábado',
+    'domingo',
+]
+
+function getTaskIcon(status: Task['status']) {
     if (status === 'done') {
         return <CheckCircleIcon fontSize="small"/>
     }
     return <PendingIcon fontSize="small"/>
 }
 
+function PlannerModal({ open, onClose, tasks }: PlannerModalProps) {
+    const startOfWeek = dayjs().startOf('week').add(1, 'day')
+
+    const endOfWeek = dayjs().endOf('week').add(1, 'day')
+
+    const tasksThisWeek = tasks.filter(task => {
+        const taskDate = dayjs(task.date)
+
+        return (
+            taskDate.isAfter(startOfWeek.subtract(1, 'day')) &&
+            taskDate.isBefore(endOfWeek.add(1, 'day'))
+        )
+    })
+
+    const groupedTasks: Record<string, Task[]> = {}
+
+    for (const task of tasksThisWeek) {
+        const dayKey = dayjs(task.date).format('dddd')
+
+        if(!groupedTasks[dayKey]) {
+            groupedTasks[dayKey] = []
+        }
+        groupedTasks[dayKey].push(task)
+    }
+}
